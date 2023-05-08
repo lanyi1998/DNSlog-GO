eb57abc545d3384189f80dd492212d219d5ef8b9
lanyi <1005197862@qq.com>
2023-05-08 15:42:25 +0800

    test: github actions

--- build.sh
+++ build.sh
@@ -6,9 +6,13 @@ mkdir -p build
 
 # Build binary for macOS
 env GOOS=darwin GOARCH=amd64 go build -o build/dnslog-macos
+zip -j build/dnslog-macos.zip build/dnslog-macos config.yaml
+
 
 # Build binary for Linux
 env GOOS=linux GOARCH=amd64 go build -o build/dnslog-linux
+zip -j build/dnslog-linux.zip build/dnslog-linux config.yaml
 
 # Build binary for Windows
 env GOOS=windows GOARCH=amd64 go build -o build/dnslog-windows.exe
+zip -j build/dnslog-windows.zip build/dnslog-windows config.yaml
