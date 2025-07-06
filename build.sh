21bf0550dcb81b50b7fa96e83d67876d7d0ced96
lanyi <31091395+lanyi1998@users.noreply.github.com>
2025-07-07 04:19:39 +0800

    build:优化构建脚本- 调整 go build命令的参数顺序，提高代码可读性 - 统一各个平台构建二进制文件的命令格式 - 保持 zip 命令的使用不变，确保打包流程一致

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 mkdir -p build
 
 # Build binary for macOS
-env GOOS=darwin GOARCH=amd64 go build cmd/server/main.go -o build/dnslog-macos
+env GOOS=darwin GOARCH=amd64 go build -o build/dnslog-macos cmd/server/main.go
 zip -j build/dnslog-macos.zip build/dnslog-macos config.yaml
 
 
 # Build binary for Linux
-env GOOS=linux GOARCH=amd64 go build cmd/server/main.go -o build/dnslog-linux
+env GOOS=linux GOARCH=amd64 go build -o build/dnslog-linux cmd/server/main.go
 zip -j build/dnslog-linux.zip build/dnslog-linux config.yaml
 
 # Build binary for Windows
-env GOOS=windows GOARCH=amd64 go build cmd/server/main.go -o build/dnslog-windows.exe
+env GOOS=windows GOARCH=amd64 go build -o build/dnslog-windows.exe cmd/server/main.go
 zip -j build/dnslog-windows.zip build/dnslog-windows.exe config.yaml
