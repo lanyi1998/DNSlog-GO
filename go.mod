b1f0defb4359a1ace02bfbcab7f244cfbf45ed81
lanyi <31091395+lanyi1998@users.noreply.github.com>
2025-07-26 04:23:37 +0800

    feat: 支持LDAP请求

--- go.mod
+++ go.mod
@@ -6,69 +6,36 @@ toolchain go1.23.5
 
 require (
 	github.com/gin-gonic/gin v1.10.1
-	github.com/gogf/gf v1.16.9
+	github.com/go-asn1-ber/asn1-ber v1.5.7
 	go.uber.org/zap v1.27.0
 	golang.org/x/net v0.41.0
+	golang.org/x/text v0.26.0
 	gopkg.in/yaml.v2 v2.2.2
 )
 
 require (
-	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/bytedance/sonic v1.13.3 // indirect
 	github.com/bytedance/sonic/loader v0.2.4 // indirect
-	github.com/clbanning/mxj v1.8.5-0.20200714211355-ff02cfb8ea28 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
-	github.com/cloudwego/iasm v0.2.0 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/fatih/color v1.12.0 // indirect
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.9 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
-	github.com/go-playground/assert/v2 v2.2.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.26.0 // indirect
-	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
-	github.com/golang/protobuf v1.5.0 // indirect
-	github.com/gomodule/redigo v1.8.5 // indirect
-	github.com/google/go-cmp v0.7.0 // indirect
-	github.com/google/gofuzz v1.0.0 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/grokify/html-strip-tags-go v0.0.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.11 // indirect
-	github.com/knz/go-libedit v1.10.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
-	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.5.2 // indirect
-	github.com/stretchr/testify v1.10.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.0 // indirect
-	github.com/yuin/goldmark v1.4.13 // indirect
-	go.opentelemetry.io/otel v1.0.0 // indirect
-	go.opentelemetry.io/otel/trace v1.0.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/arch v0.18.0 // indirect
 	golang.org/x/crypto v0.39.0 // indirect
-	golang.org/x/mod v0.25.0 // indirect
-	golang.org/x/sync v0.15.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
-	golang.org/x/telemetry v0.0.0-20240521205824-bda55230c457 // indirect
-	golang.org/x/term v0.32.0 // indirect
-	golang.org/x/text v0.26.0 // indirect
-	golang.org/x/tools v0.33.0 // indirect
-	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
-	gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	nullprogram.com/x/optparse v1.0.0 // indirect
-	rsc.io/pdf v0.1.1 // indirect
 )
--- internal/handler/multi_protocol.go
+++ internal/handler/multi_protocol.go
@@ -1,9 +1,15 @@
 package handler
 
 import (
+	"DnsLog/internal/config"
+	"DnsLog/internal/ipwry"
+	"DnsLog/internal/model"
 	"bufio"
+	"bytes"
 	"fmt"
+	"github.com/go-asn1-ber/asn1-ber"
 	"io"
+	"log"
 	"net"
 	"net/http"
 	"strings"
@@ -60,6 +66,12 @@ func isHTTP(data []byte) bool {
 func isJRMI(data []byte) bool {
 	dataStr := string(data)
 	if strings.HasPrefix(dataStr, "JRMI") {
+		return true
+	}
+	return false
+}
+func isLDAP(data []byte) bool {
+	if bytes.HasPrefix(data, []byte{48, 12, 2, 1}) {
 		return true
 	}
 	return false
@@ -122,7 +134,7 @@ func handleConnection(conn net.Conn, ginHandler http.Handler) {
 	// 创建缓冲读取器
 	reader := bufio.NewReader(conn)
 
-	peekSize := 512
+	peekSize := 4
 	data, err := reader.Peek(peekSize)
 	if err != nil && err != io.EOF {
 		conn.Close()
@@ -139,6 +151,10 @@ func handleConnection(conn net.Conn, ginHandler http.Handler) {
 	if isJRMI(data) {
 		conn.SetReadDeadline(time.Time{})
 		handleJRMI(conn, reader)
+	}
+	if isLDAP(data) {
+		conn.SetReadDeadline(time.Time{})
+		handleLDAP(conn, reader)
 	}
 	handleOtherProtocol(conn, reader)
 }
@@ -179,5 +195,237 @@ func handleJRMI(conn net.Conn, reader *bufio.Reader) {
 	// 发送 PingAck
 	_, err = conn.Write(pingAckResponse)
 	_, err = reader.Read(data)
+}
 
+// LDAPMessage LDAP请求结构
+type LDAPMessage struct {
+	MessageID  int64
+	ProtocolOp *ber.Packet
+	Controls   []*ber.Packet
+}
+
+func handleLDAP(conn net.Conn, reader *bufio.Reader) {
+	buffer := make([]byte, 4096)
+	for {
+		n, err := reader.Read(buffer)
+		if err != nil {
+			if err.Error() != "EOF" {
+				log.Printf("读取数据错误: %v\n", err)
+			}
+			break
+		}
+
+		if n > 0 {
+			processLDAPMessage(buffer[:n], conn)
+		}
+	}
+}
+
+func processLDAPMessage(data []byte, conn net.Conn) {
+	// 解析BER编码的数据
+	packet := ber.DecodePacket(data)
+	// 解析LDAP消息
+	ldapMsg, err := parseLDAPMessage(packet)
+	if err != nil {
+		return
+	}
+	// 根据操作类型处理请求
+	handleLDAPRequest(ldapMsg, conn)
+}
+
+func parseLDAPMessage(packet *ber.Packet) (*LDAPMessage, error) {
+	if len(packet.Children) < 2 {
+		return nil, fmt.Errorf("无效的LDAP消息格式")
+	}
+
+	// 消息ID
+	messageID, ok := packet.Children[0].Value.(int64)
+	if !ok {
+		return nil, fmt.Errorf("无效的消息ID")
+	}
+
+	// 协议操作
+	protocolOp := packet.Children[1]
+
+	// 控制信息（可选）
+	var controls []*ber.Packet
+	if len(packet.Children) > 2 {
+		controls = packet.Children[2].Children
+	}
+
+	return &LDAPMessage{
+		MessageID:  messageID,
+		ProtocolOp: protocolOp,
+		Controls:   controls,
+	}, nil
+}
+
+// LDAP操作类型常量
+const (
+	ApplicationBindRequest      = 0
+	ApplicationBindResponse     = 1
+	ApplicationUnbindRequest    = 2
+	ApplicationSearchRequest    = 3
+	ApplicationSearchResultDone = 5
+)
+
+func handleLDAPRequest(msg *LDAPMessage, conn net.Conn) {
+	opType := msg.ProtocolOp.Tag
+
+	switch opType {
+	case ApplicationBindRequest:
+		handleBindRequest(msg, conn)
+	case ApplicationSearchRequest:
+		handleSearchRequest(msg, conn)
+	}
+}
+
+// BindRequest LDAP绑定请求
+type BindRequest struct {
+	Version        int64
+	Name           string
+	Authentication interface{}
+}
+
+func parseBindRequest(packet *ber.Packet) (*BindRequest, error) {
+	if len(packet.Children) < 3 {
+		return nil, fmt.Errorf("无效的绑定请求格式")
+	}
+
+	version, ok := packet.Children[0].Value.(int64)
+	if !ok {
+		return nil, fmt.Errorf("无效的版本号")
+	}
+
+	name, ok := packet.Children[1].Value.(string)
+	if !ok {
+		return nil, fmt.Errorf("无效的绑定名称")
+	}
+
+	return &BindRequest{
+		Version:        version,
+		Name:           name,
+		Authentication: packet.Children[2].Value,
+	}, nil
+}
+
+// 处理绑定请求
+func handleBindRequest(msg *LDAPMessage, conn net.Conn) {
+	_, err := parseBindRequest(msg.ProtocolOp)
+	if err != nil {
+		return
+	}
+	// 发送绑定响应
+	response := createBindResponse(msg.MessageID, 0, "", "绑定成功")
+	sendResponse(response, conn)
+}
+
+func createBindResponse(messageID int64, resultCode int64, matchedDN, diagnosticMessage string) *ber.Packet {
+	response := ber.Encode(ber.ClassUniversal, ber.TypeConstructed, ber.TagSequence, nil, "LDAP Response")
+	response.AppendChild(ber.NewInteger(ber.ClassUniversal, ber.TypePrimitive, ber.TagInteger, messageID, "MessageID"))
+
+	bindResponse := ber.Encode(ber.ClassApplication, ber.TypeConstructed, ApplicationBindResponse, nil, "Bind Response")
+	bindResponse.AppendChild(ber.NewInteger(ber.ClassUniversal, ber.TypePrimitive, ber.TagEnumerated, resultCode, "Result Code"))
+	bindResponse.AppendChild(ber.NewString(ber.ClassUniversal, ber.TypePrimitive, ber.TagOctetString, matchedDN, "Matched DN"))
+	bindResponse.AppendChild(ber.NewString(ber.ClassUniversal, ber.TypePrimitive, ber.TagOctetString, diagnosticMessage, "Diagnostic Message"))
+
+	response.AppendChild(bindResponse)
+	return response
+}
+
+func sendResponse(response *ber.Packet, conn net.Conn) {
+	data := response.Bytes()
+	_, err := conn.Write(data)
+	if err != nil {
+		log.Printf("发送响应错误: %v\n", err)
+	}
+}
+
+// handleSearchRequest 处理搜索请求
+func handleSearchRequest(msg *LDAPMessage, conn net.Conn) {
+	searchReq, err := parseSearchRequest(msg.ProtocolOp)
+	if err != nil {
+		return
+	}
+
+	if strings.HasPrefix(searchReq.BaseObject, "s=") {
+		username := searchReq.BaseObject[2:]
+		for k, v := range config.Config.User {
+			if v == username {
+				ipStr := strings.Split(conn.RemoteAddr().String(), ":")[0]
+				IpLocation, _ := ipwry.Query(ipStr)
+				model.UserDnsDataMap.Set(k, model.DnsInfo{
+					Type:       "LDAP",
+					Subdomain:  username,
+					Ipaddress:  ipStr,
+					Time:       time.Now().Unix(),
+					IpLocation: IpLocation,
+				})
+				break
+			}
+		}
+	}
+
+	// 发送搜索完成响应
+	response := createSearchDoneResponse(msg.MessageID, 0, "", "搜索完成")
+	sendResponse(response, conn)
+}
+
+type SearchRequest struct {
+	BaseObject   string
+	Scope        int64
+	DerefAliases int64
+	SizeLimit    int64
+	TimeLimit    int64
+	TypesOnly    bool
+	Filter       *ber.Packet
+	Attributes   []string
+}
+
+func parseSearchRequest(packet *ber.Packet) (*SearchRequest, error) {
+	if len(packet.Children) < 7 {
+		return nil, fmt.Errorf("无效的搜索请求格式")
+	}
+
+	baseObject, _ := packet.Children[0].Value.(string)
+	scope, _ := packet.Children[1].Value.(int64)
+	derefAliases, _ := packet.Children[2].Value.(int64)
+	sizeLimit, _ := packet.Children[3].Value.(int64)
+	timeLimit, _ := packet.Children[4].Value.(int64)
+	typesOnly, _ := packet.Children[5].Value.(bool)
+	filter := packet.Children[6]
+
+	var attributes []string
+	if len(packet.Children) > 7 {
+		attrList := packet.Children[7]
+		for _, attr := range attrList.Children {
+			if attrName, ok := attr.Value.(string); ok {
+				attributes = append(attributes, attrName)
+			}
+		}
+	}
+
+	return &SearchRequest{
+		BaseObject:   baseObject,
+		Scope:        scope,
+		DerefAliases: derefAliases,
+		SizeLimit:    sizeLimit,
+		TimeLimit:    timeLimit,
+		TypesOnly:    typesOnly,
+		Filter:       filter,
+		Attributes:   attributes,
+	}, nil
+}
+
+func createSearchDoneResponse(messageID int64, resultCode int64, matchedDN, diagnosticMessage string) *ber.Packet {
+	response := ber.Encode(ber.ClassUniversal, ber.TypeConstructed, ber.TagSequence, nil, "LDAP Response")
+	response.AppendChild(ber.NewInteger(ber.ClassUniversal, ber.TypePrimitive, ber.TagInteger, messageID, "MessageID"))
+
+	searchDone := ber.Encode(ber.ClassApplication, ber.TypeConstructed, ApplicationSearchResultDone, nil, "Search Result Done")
+	searchDone.AppendChild(ber.NewInteger(ber.ClassUniversal, ber.TypePrimitive, ber.TagEnumerated, resultCode, "Result Code"))
+	searchDone.AppendChild(ber.NewString(ber.ClassUniversal, ber.TypePrimitive, ber.TagOctetString, matchedDN, "Matched DN"))
+	searchDone.AppendChild(ber.NewString(ber.ClassUniversal, ber.TypePrimitive, ber.TagOctetString, diagnosticMessage, "Diagnostic Message"))
+
+	response.AppendChild(searchDone)
+	return response
 }
